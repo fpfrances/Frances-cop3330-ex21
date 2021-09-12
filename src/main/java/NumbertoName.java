e92ab5cfe4392c4983011f38aa1e12df34d3e274
fpfrances <fpfrances@hotmail.com>
2021-09-11 21:03:48 -0400

    Initial commit

---
+++ src/main/java/NumbertoName.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+import java.util.Scanner;
+public class NumbertoName {
+    public String getMonth(int month){
+        switch (month) {
+            case 1:  return "January";
+            case 2:  return "February";
+            case 3:  return "March";
+            case 4:  return "April";
+            case 5:  return "May";
+            case 6:  return "June";
+            case 7:  return "July";
+            case 8:  return "August";
+            case 9:  return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+            default: return "Error!";
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        NumbertoName ntn = new NumbertoName();
+        System.out.print("Please enter the number of month: ");
+        int month = sc.nextInt();
+        String result = ntn.getMonth(month);
+        if(month>0 && month<=12)
+            System.out.print("The name of month is "+result+".");
+        else
+            System.out.print(result+" Please enter correct month number between 1 to 12.");
+    }
+}
